f9cd2773a30067b01f66e08ca726484296f1b4c4
lawwton <alfredo@emitrom.com>
2015-02-22 22:03:17 -0500

    Creating packaging file, setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
-setup.py
+from distutils.core import setup
+
+setup(
+    name='integra-sdk',
+    version='0.1.0',
+    author='Emitrom Inc.',
+    author_email='support@emitrom.com',
+    packages=['integra'],
+    url='http://pypi.python.org/pypi/integra-sdk/',
+    license='LICENSE.txt',
+    description='Integra SDK',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "requests == 2.5.1",
+        "lxml == 3.4.2",
+    ],
+)
